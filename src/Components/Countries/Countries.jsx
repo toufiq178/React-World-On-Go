21456a644b49292c18d2180f8296a385085ba9e1
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-03-15 00:25:18 +0600

    display api visites not visites set

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,15 +1,23 @@
 import React, { use } from 'react';
+import Country from './Country/Country';
+import './Countries.css';
 
-const Countries = ({countriesPromise}) => {
+const Countries = ({ countriesPromise }) => {
 
-    const countriesData = use(countriesPromise) ;
+    const countriesData = use(countriesPromise);
     const countries = countriesData.countries
-    console.log(countries);
-    
+    // console.log(countries);
+
 
     return (
-        <div>
+        <div >
             <h1>Total Countries :  {countries.length}</h1>
+
+            <div className='card-container'>
+                {
+                    countries.map(country => <Country key={country.ccn3.ccn3} country={country}></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/Components/Countries/Country/Country.jsx
+++ src/Components/Countries/Country/Country.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Country.css'
 
-const Country = () => {
+const Country = ({ country }) => {
+
+    // console.log(country.area.area);
+
+    const [visited, setVisited] = useState(false)
+
+    function handlerVisited() {
+
+        // if (visited) {
+        //     setVisited(false)
+        // }else{
+        //     setVisited(true)
+        // }
+
+        setVisited(visited ? false : true)
+
+        // setVisited(!visited)
+    }
+
     return (
-        <div>
-            
+        // <div className={`card ${visited ? 'visited' : 'not-visited'}`}>
+        <div className={`card ${visited && 'visited'}`}>
+
+            <button onClick={handlerVisited}>{visited ? "Visited" : "Not Visited"}</button>
+
+            <div>
+                <h2>Name : {country.name.common}</h2>
+                <p>Population : {country.population.population}</p>
+                <p>Region : {country.region.region}</p>
+                <p>Area : {country.area.area}  </p>
+                <p>{country.area.area > 300000 ? "Big Country" : "Small Country"}</p>
+            </div>
+
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
         </div>
     );
 };
